867743b81692241e685725c115db0d5b33bf806a
knchandra <chandrashekar.kn@3icube.com>
2013-11-19 18:20:38 +0530

    updated CreateBilling

--- hybridframework/src/test/java/com/qspiders/hybridframework/settings/CreateBilling.java
+++ hybridframework/src/test/java/com/qspiders/hybridframework/settings/CreateBilling.java
@@ -7,6 +7,7 @@ import com.qspiders.hybridframework.testbase.BaseClass;
 public class CreateBilling extends BaseClass{
 	@Test
 	public void testCreateBilling() {
-		System.out.println("Create Billing");
+		System.out.println("start Create Billing");
+		System.out.println("end Create Billing");
 	}
 }
